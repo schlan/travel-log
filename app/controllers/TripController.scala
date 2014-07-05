c29d14be3de120d00208a4bbe759c1910b657b51
Mathias Kub <git@makubi.at>
2014-07-05 15:44:53 +0200

    Reorganized templates

--- app/controllers/TripController.scala
+++ app/controllers/TripController.scala
@@ -12,7 +12,7 @@ class TripController(tripService: TripService, imageService: ImageService) exten
 
   def getTripById(tripId: String) = Action {
     tripService.getTripById(tripId) match {
-      case Some(trip) => Ok(views.html.touroverview("Tracks:", tripService.getAllTripsForNavBar, trip))
+      case Some(trip) => Ok(views.html.tour.touroverview("Tracks:", tripService.getAllTripsForNavBar, trip))
       case None => NotFound("getTripById: Err0r: Not found :(")
     }
 
@@ -26,7 +26,7 @@ class TripController(tripService: TripService, imageService: ImageService) exten
         case Success(dId) =>{
           (tripService.getTripById(tripId), tripService.getDayTour(tripId, dId)) match {
             case (Some(trip), Some(dayTour)) => {
-              Ok(views.html.tour("DayTour:", tripService.getAllTripsForNavBar(),trip, dayTour))
+              Ok(views.html.tour.tour("DayTour:", tripService.getAllTripsForNavBar(),trip, dayTour))
             }
             case (_, _) => NotFound("getDayTour: Err0r: Not found :(")
           }
